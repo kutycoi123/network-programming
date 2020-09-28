741423ece984e7ff11e203107c8f35a6671f6c4c
kutycoi123 <kutycoi123@gmail.com>
2020-09-27 22:22:28 -0700

    Added code for constructor in response.cpp

--- rsp/response.cpp
+++ rsp/response.cpp
@@ -11,6 +11,24 @@ model::Response::Response(char opt, const char* msg)
 :_opt(opt)
 {
     // TODO: Complete
+    std::string msg_str(msg);
+    _ret = msg[0];
+    _len = utils::bytesToInt(msg_str.substr(1, 4).c_str());
+    if (_ret != RET_SUCCESS || _opt == OPT_ADD_ITEM || _opt == OPT_ADD_ITEM
+        || _opt == OPT_CLR_SET || _opt == OPT_REM_SET) 
+      return;
+    int len = msg_str.length();
+    if (_opt == OPT_CRT_SET || _opt == OPT_GET_SETS) {
+      for (int i = 5; i < len; i+=32) {
+         _uuids.push_back(msg_str.substr(i, 32)); 
+      }
+    }else if (_opt == OPT_GET_ITEMS){
+      for (int i = 5; i < len; i+=4) {
+         _items.push_back(utils::bytesToInt(msg_str.substr(i, 4).c_str()));
+      }
+    }else if (_opt == OPT_GET_SIZE) {
+      _set_size = utils::bytesToInt(msg_str.substr(5, 4).c_str());
+    }
 }
 
 model::Response::Response(const Response& orig)
